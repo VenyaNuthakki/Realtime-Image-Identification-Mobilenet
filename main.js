362d32e86f0bb891e4b9fbd5b7bb8ce9938fe4a1
VenyaNuthakki <71914805+VenyaNuthakki@users.noreply.github.com>
2021-09-18 11:29:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier=ml5.imageClassifier("MobileNet", modelloaded);
 }
 
+function modelloaded() {
+  console.log("modelloaded");
+}
 
+function draw() {
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, get_result);
+}
 
+function get_result(error,results) {
+  if (error){
+    console.log("error")
+  }
+  else{
+    console.log(results);
+    document.getElementById("object_name").innerHTML=results[0].label;
+    document.getElementById("object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+  }
+} 
